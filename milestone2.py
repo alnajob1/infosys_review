b82834825d2af083ab94d09c053a18693cd9f3ad
alnajob1 <alnajob123@gmail.com>
2026-02-16 20:42:36 +0530

    Add files via upload

---
+++ milestone2.py
@@ -0,0 +1,48 @@
+import pandas as pd
+from textblob import TextBlob
+import matplotlib.pyplot as plt
+
+
+
+def get_sentiment(text):
+    polarity = TextBlob(str(text)).sentiment.polarity
+
+    if polarity > 0:
+        return "Positive", polarity
+    elif polarity < 0:
+        return "Negative", polarity
+    else:
+        return "Neutral", polarity
+
+
+if __name__ == "__main__":
+    
+    df = pd.read_csv("Milestone1_Cleaned_Feedback.csv")
+
+    
+    df[["sentiment", "confidence_score"]] = df["clean_feedback"].apply(lambda x: pd.Series(get_sentiment(x)))
+
+    
+    df.to_csv("Milestone2_Sentiment_Results_new.csv", index=False)
+
+    print("Milestone 2 is completed successfully!!")
+
+    #plotting sentiment distribution
+
+    sentiment_counts=df['sentiment'].value_counts( )
+    plt.figure(figsize=(8,5))
+    colors=['green','red','gray']
+    sentiment_counts.plot(kind='bar',color=colors)
+    plt.title('How Customer Feel - Sentiment Summary',fontsize=14)
+    plt.xlabel('Sentiment',fontsize=12)
+    plt.ylabel('Number of Reviews',fontsize=12)
+    plt.xticks(rotation=0)
+
+    for i,count in enumerate(sentiment_counts):
+        plt.text(i,count+20,str(count),ha="center",fontsize=10)
+
+    plt.savefig('Sentiment-bar-chart.png',dpi=100,bbox_inches='tight')
+    plt.show()
+    print("Bar charrt saved as :Sentiment_bar_chart.png")
+
+    print(df[["clean_feedback", "sentiment", "confidence_score"]].head())
